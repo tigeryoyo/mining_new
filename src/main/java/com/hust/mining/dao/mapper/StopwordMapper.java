843f01e9fba6f2dd390b2f191106e9d2b1282d74
466413559 <466413559@qq.com>
2017-05-07 17:38:42 +0800

    stopword功能页面的实现 stopword数据库代码的生成

---
+++ src/main/java/com/hust/mining/dao/mapper/StopwordMapper.java
@@ -0,0 +1,30 @@
+package com.hust.mining.dao.mapper;
+
+import com.hust.mining.model.Stopword;
+import com.hust.mining.model.StopwordExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface StopwordMapper {
+    int countByExample(StopwordExample example);
+
+    int deleteByExample(StopwordExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Stopword record);
+
+    int insertSelective(Stopword record);
+
+    List<Stopword> selectByExample(StopwordExample example);
+
+    Stopword selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Stopword record, @Param("example") StopwordExample example);
+
+    int updateByExample(@Param("record") Stopword record, @Param("example") StopwordExample example);
+
+    int updateByPrimaryKeySelective(Stopword record);
+
+    int updateByPrimaryKey(Stopword record);
+}
