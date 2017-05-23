020df2949167fceac0728c9b6e8f284188e9052e
kaiwenT <1043595701@qq.com>
2017-05-23 23:37:11 +0800

    更新获取URL排序

--- src/main/java/com/hust/mining/urltags/URLTool.java
+++ src/main/java/com/hust/mining/urltags/URLTool.java
@@ -16,8 +16,10 @@ import com.hust.mining.util.ExcelUtil;
 public class URLTool {
 	
 	public static void main(String[] args) {
-		String fileName = "E:/测试数据/汪哥交流以及数据5.11/汪哥交流以及数据5.11/准数据/四川戒毒管理局2017.5.4~5.10准数据.xlsx";//C:/Users/tankai/Desktop/result.xls
+		String fileName = "F:/四川项目相关/汪哥交流以及数据5.11/汪哥交流以及数据5.11/准数据/四川戒毒管理局2017.5.4~5.10准数据.xlsx";//C:/Users/tankai/Desktop/result.xls
 		new URLTool().statisticUrl(fileName);
+		
+//		System.out.println(CommonUtil.getPrefixUrl("http://weibo.com/3800181655/F1Bb5jIMI"));
 	}
 	/**
 	 * 统计准数据文件url中出现次数最多的网站
@@ -25,30 +27,31 @@ public class URLTool {
 	 */
 	public void statisticUrl(String fileName){
 		Map<String,Integer> urlNumberMap = new HashedMap();
-		try {
-			List<String[]> dataList = ExcelUtil.read(fileName);
-			
+//		try {
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
+//			List<String[]> dataList = ExcelUtil.read(fileName);
+			List<String> dataList = ExcelReader.read(fileName,5);
+			dataList.remove("链接");
 			if(dataList.isEmpty()){
 				return;
 			}
 			System.out.println(dataList.size());
-			for(String[] strs : dataList){
-				String url = strs[5].trim();
+			for(String  url : dataList){
+//				String url = strs[5].trim();
 				System.out.println(url);
-				if(!url.equals("")){
+				if(!url.trim().equals("")){
 					url = CommonUtil.getPrefixUrl(url);
 					countUrl(url,urlNumberMap);
 				}
 			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		for(String url : urlNumberMap.keySet()){
-			System.out.println(url+"    "+urlNumberMap.get(url));
-		}
 		
+	
 		System.out.println("排序后-----");
+		System.out.println("网站url\t\t\t数量");
 		sort(urlNumberMap);
 	}
 
@@ -61,7 +64,7 @@ public class URLTool {
 		}
 		
 	}
-	
+	//对url按数量进行排序
 	public void sort( Map<String, Integer> urlNumberMap){
 		List<Map.Entry<String, Integer>> urls = new ArrayList<>(urlNumberMap.entrySet());
 		Collections.sort(urls, new Comparator<Map.Entry<String, Integer>>() {
@@ -74,7 +77,7 @@ public class URLTool {
 		});
 		
 		for (Entry<String, Integer> entry : urls) {
-			System.out.println(entry.getKey()+"    "+entry.getValue());
+			System.out.println(entry.getKey()+"\t\t"+entry.getValue());
 		}
 	}
 }
