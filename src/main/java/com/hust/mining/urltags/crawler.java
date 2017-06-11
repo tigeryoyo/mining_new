c9ed69e43b8b8cf65baddf55fbcb3a9a9739d44f
Administrator <Administrator@USER-20160621QP>
2017-06-11 20:18:37 +0800

    修改

--- src/main/java/com/hust/mining/urltags/crawler.java
+++ src/main/java/com/hust/mining/urltags/crawler.java
@@ -7,10 +7,14 @@ import cn.edu.hfut.dmic.webcollector.model.CrawlDatums;
 import cn.edu.hfut.dmic.webcollector.model.Page;
 import cn.edu.hfut.dmic.webcollector.plugin.berkeley.BreadthCrawler;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class crawler extends BreadthCrawler {
 
@@ -25,47 +29,82 @@ public class crawler extends BreadthCrawler {
 
     public void visit(Page page, CrawlDatums next) {
     
-            Document doc = page.getDoc();   
             //如果是法制网、四川法制报、司法部，用以下规则
             if(page.matchUrl("http://www.legaldaily.com.cn/.*")||
             		page.matchUrl("http://dzb.scfzbs.com/.*")||
             		page.matchUrl("http://www.moj.gov.cn/.*"))
             {
             	String content = page.select("p").text();
-        	   System.out.println("content:\n" + content);
+        	    System.out.println("content:\n" + content);
             }
-            //如果是四川法制网，用以下规则
+            //如果是，用以下规则
             if(page.matchUrl("http://www.scfz.org/.*"))
             {
             	String content = page.select("td[class=content_word]").text();
         	   System.out.println("content:\n" + content);
             }
-            //如果是人民网，用以下规则
-            if(page.matchUrl("http://.*people.com.cn/.*"))
-            {
-            	String content = page.select("div[class=fl text_con_left]").text();
-        	   System.out.println("content:\n" + content);
-            }
-         
+           
     }
     
+  //爬取法制网、四川法制报、司法部
+    private static List<String> getContent(String url) throws IOException {  
+        // TODO Auto-generated method stub          
+    	 Document doc;  
+         doc=Jsoup.connect(url).get(); 
+         //标题
+         Element title = doc.select("title").first();
+         String tString = title.text();
+         String strings[] = tString.split("-");
+         System.out.println(strings[0]);
+ 		//正文
+         Elements pElements=doc.select("p");
+         for (Element element : pElements) {
+ 			System.out.println(element.text());
+ 		}
+ 		return null;          
+    }  
+    
+  //爬取四川法制网
+    private static List<String> getContent1(String url) throws IOException {  
+        // TODO Auto-generated method stub          
+    	 Document doc;  
+         doc=Jsoup.connect(url).get(); 
+         //标题
+         Element title = doc.select("title").first();
+        	System.out.println(title.text());
+ 		//正文
+         Elements pElements=doc.select("p");
+         for (Element element : pElements) {
+ 			String pString = element.text();
+ 			String[] ss = pString.split(" ");
+ 			for (String string : ss) {
+				System.out.println(string);
+			}
+ 		}
+ 		return null;          
+    }  
+    
+    //爬取人民网
+    private static List<String> getContent2(String url) throws IOException {  
+        // TODO Auto-generated method stub          
+        Document doc;  
+        doc=Jsoup.connect(url).get(); 
+        //标题
+        Element title = doc.select("h1").first();
+       	System.out.println(title.text());
+		//正文
+        Elements pElements=doc.select("div.box_con>p");
+        for (Element element : pElements) {
+			System.out.println(element.text());
+		}
+		return null;          
+    }  
 
     public static void main(String[] args) throws Exception {
-    	crawler crawler = new crawler("crawl", true);
-        /*线程数*/
-    	List<String> list= new ArrayList<String>();
-    	list.add("http://www.legaldaily.com.cn/index_article/content/2017-06/11/content_7200042.htm?node=5955");
-    	list.add("http://www.scfz.org/news/html/72-29/29973.htm");
-    	list.add("http://dzb.scfzbs.com/shtml/scfzb/20170609/50658.shtml");
-    	list.add("http://www.moj.gov.cn/index/content/2017-05/31/content_7186914.htm?node=86528");
-    	list.add("http://politics.people.com.cn/n1/2017/0610/c1001-29331177.html");
     	
-    	
-    	for (String string : list) {
-			crawler.addSeed(string);
-		}
-    	
-        crawler.start(1);
+    	getContent("http://www.legaldaily.com.cn/index_article/content/2017-06/11/content_7200041.htm?node=5955");
+    	//人民网
+    //	getContent2("http://politics.people.com.cn/n1/2017/0610/c1001-29331177.html");
        
     }
 
