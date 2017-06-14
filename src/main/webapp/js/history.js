4a98558fa80c3a52559b9d93162b6fe5cb3ca144
tigeryoyo <m15527081139@163.com>
2017-06-14 13:05:52 +0800

    前台bug修改

--- src/main/webapp/js/history.js
+++ src/main/webapp/js/history.js
@@ -1,8 +1,6 @@
 /**
  * Created by Administrator on 2016/12/18.
  */
-// document.write('<script type="text/javascript"
-// src="js/cluster_details.js"></script>');
 function historyRecord() {
 	$.ajax({
 		type : "post",
@@ -47,14 +45,9 @@ function historyData(rid) {
 				for (var i = 0; i < items.length - 1; i++) {
 					// items第一行存储index，故从i+1读起
 					item = items[i + 1];
-					rows = '<tr><td height="32" align="center"><input type="checkbox" style="width:20px;height:20px" class="' + i
-						+ '"/></td><td height="32" align="center"><a href="javascript:;" onclick="showClusterDetails('
-						// + item[indexOfUrl]
-						// + '
-						+ i + ',\''
-						// + item[indexOfUrl]
-						+ rid + '\',' + item[0] + ')">' + item[indexOfTitle] + '</a></td><td height="32" align="center">' + item[indexOfTime] + '</td><td height="32" align="center">'
-						+ '<a href="javascript:;" onclick="toPaint(' + i + ',\'' + item[indexOfTitle].replace(/\"/g, " ").replace(/\'/g, " ") + '\')">' + item[0] + '</a>' + '</td></tr>';
+					rows = '<tr><td height="32" align="center"><input type="checkbox" class="' + i + '"/></td><td height="32" align="center"><a href="' + item[indexOfUrl] + '" target="_blank">'
+						+ item[indexOfTitle] + '</a></td><td height="32" align="center">' + item[indexOfTime] + '</td><td height="32" align="center">' + '<a href="javascript:;" onclick="toPaint(' + i
+						+ ',\'' + item[indexOfTitle].replace(/\"/g, " ").replace(/\'/g, " ") + '\')">' + item[0] + '</a>' + '</td></tr>';
 					$('.summary_tab table').append(rows);
 
 				}
@@ -62,8 +55,8 @@ function historyData(rid) {
 				alert(msg.result);
 			}
 		},
-		error : function(msg) {
-			alert(msg.result);
+		error : function() {
+			alert("请求失败");
 		}
 	})
 }
@@ -75,7 +68,7 @@ function buildStandardData() {
 	});
 }
 
-/* 删除历史记录 */
+/* 删除 */
 function historyDel() {
 	$(".summary_up table tr").unbind('click').on("click", "img", function() {
 		var result_id = $(this).attr("id");
@@ -137,7 +130,7 @@ function historyReset() {
 	})
 }
 
-/* 合并类簇 */
+/* 合并 */
 function addLayData() {
 	var sets = [];
 	$(".summary_tab input:checked").each(function(i) {
@@ -181,8 +174,7 @@ function freshData() {
 					// items第一行存储index，故从i+1读起
 					item = items[i + 1];
 					console.log(item);
-					rows = '<tr><td height="32" align="center"><input type="checkbox" style="width:20px;height:20px" class="' + i
-						+ '"/></td><td height="32" align="center"><a href="javascript:;" onclick="showClusterDetails(' + i + ',\'' + $('.summary_up table tr img').attr("id") + '\',' + item[0] + ')">'
+					rows = '<tr><td height="32" align="center"><input type="checkbox" class="' + i + '"/></td><td height="32" align="center"><a href="' + item[indexOfUrl] + '" target="_blank">'
 						+ item[indexOfTitle] + '</a></td><td height="32" align="center">' + item[indexOfTime] + '</td><td height="32" align="center">' + '<a href="javascript:;" onclick="toPaint(' + i
 						+ ',\'' + item[indexOfTitle].replace(/\"/g, " ").replace(/\'/g, " ") + '\')">' + item[0] + '</a>' + '</td></tr>';
 					$('.summary_tab table').append(rows);
@@ -198,7 +190,7 @@ function freshData() {
 	});
 }
 
-/* 删除选中类簇 */
+/* 删除 */
 function deleteLayData() {
 	var sets = [];
 	$(".summary_tab input:checked").each(function(i) {
@@ -224,7 +216,6 @@ function deleteLayData() {
 	});
 }
 
-// 全选所有聚类历史结果
 $(function() {
 	$("#historyAll").click(function() {
 		if (this.checked) {
