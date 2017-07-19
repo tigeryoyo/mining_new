fc148f87f5b1f559b342a032becd4ded8c365dc8
Administrator <Administrator@USER-20160621QP>
2017-07-19 17:36:51 +0800

    修改泛数据显示聚类详情的BUG

--- src/main/webapp/js/history.js
+++ src/main/webapp/js/history.js
@@ -3,7 +3,21 @@
  */
 // document.write('<script type="text/javascript"
 // src="js/cluster_details.js"></script>');
-//显示聚类的结果表
+
+function setCookie_resultId(value){
+	var Days = 1; // 此 cookie 将被保存 1 天
+	var exp　= new Date();
+	exp.setTime(exp.getTime() +Days*24*60*60*1000);
+	document.cookie = "resultId="+ escape (value) + ";expires=" + exp.toGMTString();
+}
+
+function getCookie(name) {
+	var arr =document.cookie.match(new RegExp("(^|)"+name+"=([^;]*)(;|$)"));
+	if(arr !=null) 
+		return unescape(arr[2]); 
+	return null;
+}
+//显示聚类的结果表, 目前没有用到
 function historyRecord() {
 	$.ajax({
 		type : "post",
@@ -16,6 +30,7 @@ function historyRecord() {
 					if(i==0)
 					{
 						historyData(item.rid);
+						setCookie_resultId(item.rid);
 						var issueId = getCookie("issueId");
 						showExtensiveIssueName(issueId);
 					}
@@ -34,6 +49,7 @@ function historyRecord() {
 }
 historyRecord();
 
+//显示当前任务名字
 function showExtensiveIssueName(issueId) {
 	$.ajax({
 		type : "post",
@@ -85,7 +101,8 @@ function historyData(rid) {
 						+ i + ',\''
 						// + item[indexOfUrl]
 						+ rid + '\',' + item[0] + ')">' + item[indexOfTitle] + '</a></td><td height="32" align="center">' + item[indexOfTime] + '</td><td height="32" align="center">'
-						+ '<a href="javascript:;" onclick="toPaint(' + i + ',\'' + item[indexOfTitle].replace(/\"/g, " ").replace(/\'/g, " ") + '\')">' + item[0] + '</a>' + '</td></tr>';
+						//添加画图的代码为：'<a href="javascript:;" onclick="toPaint(' + i + ',\'' + item[indexOfTitle].replace(/\"/g, " ").replace(/\'/g, " ") + '\')">' + item[0] + '</a>'
+						+  item[0]  + '</td></tr>';
 					$('.summary_tab table').append(rows);
 
 				}
@@ -97,6 +114,40 @@ function historyData(rid) {
 			alert(msg.result);
 		}
 	})
+}
+
+function freshData() {
+	var rid = getCookie("resultId");
+	$.ajax({
+		type : "post",
+		url : "/result/getCountResult",
+		success : function(msg) {
+			$('.summary_tab table tr:not(:first)').html('');
+			if (msg.status == "OK") {
+				// alert("删除成功");
+				var items = msg.result;
+
+				var indexOfTitle = parseInt(items[0][0]) + 1;
+				var indexOfUrl = parseInt(items[0][1]) + 1;
+				var indexOfTime = parseInt(items[0][2]) + 1;
+				for (var i = 0; i < items.length - 1; i++) {
+					// items第一行存储index，故从i+1读起
+					item = items[i + 1];
+					//console.log(item);
+					rows = '<tr><td height="32" align="center"><input type="checkbox" style="width:20px;height:20px" class="' + i
+						+ '"/></td><td height="32" align="center"><a href="javascript:;" onclick="showClusterDetails(' + i + ',\'' + rid + '\',' + item[0] + ')">'
+						+ item[indexOfTitle] + '</a></td><td height="32" align="center">' + item[indexOfTime] + '</td><td height="32" align="center">' + item[0] + '</td></tr>';
+					$('.summary_tab table').append(rows);
+
+				}
+			} else {
+				alert(msg.result);
+			}
+		},
+		error : function(msg) {
+			alert(msg.result);
+		}
+	});
 }
 
 function buildStandardData() {
@@ -139,18 +190,20 @@ function historyDel() {
 
 //下载准数据
 function download(){
-	alert("s");
 	var form = $('<form method="POST" action="/file/download">');
 	$('body').append(form);
 	form.submit(); // 自动提交
 }
 
+/*准数据页面中不需要出图
 function toPaint(currentSet, title) {
 	setCookie('currentSet', currentSet);
 	setCookie('title', title);
 	baseAjax("data_results");
 }
+*/
 
+//重置结果
 function historyReset() {
 	$(".summary_up table tr").unbind('click').on("click", "button", function() {
 		var result_id = $(this).attr("id");
@@ -202,39 +255,7 @@ function addLayData() {
 	});
 }
 
-function freshData() {
-	$.ajax({
-		type : "post",
-		url : "/result/getCountResult",
-		success : function(msg) {
-			$('.summary_tab table tr:not(:first)').html('');
-			if (msg.status == "OK") {
-				// alert("删除成功");
-				var items = msg.result;
 
-				var indexOfTitle = parseInt(items[0][0]) + 1;
-				var indexOfUrl = parseInt(items[0][1]) + 1;
-				var indexOfTime = parseInt(items[0][2]) + 1;
-				for (var i = 0; i < items.length - 1; i++) {
-					// items第一行存储index，故从i+1读起
-					item = items[i + 1];
-					//console.log(item);
-					rows = '<tr><td height="32" align="center"><input type="checkbox" style="width:20px;height:20px" class="' + i
-						+ '"/></td><td height="32" align="center"><a href="javascript:;" onclick="showClusterDetails(' + i + ',\'' + $('.summary_up table tr button').attr("id") + '\',' + item[0] + ')">'
-						+ item[indexOfTitle] + '</a></td><td height="32" align="center">' + item[indexOfTime] + '</td><td height="32" align="center">' + '<a href="javascript:;" onclick="toPaint(' + i
-						+ ',\'' + item[indexOfTitle].replace(/\"/g, " ").replace(/\'/g, " ") + '\')">' + item[0] + '</a>' + '</td></tr>';
-					$('.summary_tab table').append(rows);
-
-				}
-			} else {
-				alert(msg.result);
-			}
-		},
-		error : function(msg) {
-			alert(msg.result);
-		}
-	});
-}
 
 /* 删除选中类簇 */
 function deleteLayData() {
